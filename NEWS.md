afa366c9bf16755416c01ff087c41724c7d209ba
Marco Ramljak <45019651+R-ramljak@users.noreply.github.com>
2021-06-18 15:21:29 +0200

    passing all checks for first function

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# MNOanalyze 0.0.1
+
+* Added a `NEWS.md` file to track changes to the package.
--- R/pop_gen.R
+++ R/pop_gen.R
@@ -21,22 +21,24 @@
 #'     `area.elevation` raster layer of the area with elevation values on the tile level
 #'
 #' @examples
-#' # tile.num <- 100 # number of tiles
+#' tile.num <- 100 # number of tiles
 #' base.tile.size <- 100 # size of a single tile
 #' city.num <- 1 # number of single cities
 #' city.size <- 10 # size of a single city polygon
 #' hole.num <- 1 # number of single holes
 #' hole.size <- 1 # size of a single hole polygon
-#' pop.dist.df <- tibble(type = c("Urban", "Hole", "Rural"),
-#'                       expression = c("ReIns::rtpareto(shape = 0.05, scale = 10, endpoint = 200",
-#'                                     "ReIns::rtpareto(shape = 0.7, scale = 0.1, endpoint = 20",
-#'                                     "ReIns::rtpareto(shape = 0.5, scale = 0.1, endpoint = 100"))
-#'                                           area <- pop_gen(tile.num, base.tile.size,
-#'                                                   city.num, city.size, city.shape = "SQUARE",
-#'                                                   hole.num, hole.size, hole.shape = "SQUARE",
-#'                                                   pop.dist.df)
+#' pop.dist.df <- tibble::tibble(type = c("Urban", "Hole", "Rural"),
+#'                               expression = c("ReIns::rtpareto(shape = 0.05, scale = 10, endpoint = 200",
+#'                                              "ReIns::rtpareto(shape = 0.7, scale = 0.1, endpoint = 20",
+#'                                              "ReIns::rtpareto(shape = 0.5, scale = 0.1, endpoint = 100"))
+#'area <- pop_gen(tile.num, base.tile.size,
+#'                city.num, city.size, city.shape = "SQUARE",
+#'                hole.num, hole.size, hole.shape = "SQUARE",
+#'                pop.dist.df)
 #' @export
 #' @importFrom dplyr "%>%"
+#'
+#'
 
 
 
@@ -69,78 +71,78 @@ pop_gen <- function(tile.num,
   poly.size <- sqrt(tile.num) * base.tile.size
 
   # build basis polygon
-  area.polygon <- st_polygon(list(rbind(c(0, 0), c(poly.size, 0), c(poly.size, poly.size), c(0, poly.size), c(0, 0)))) %>%
-    st_sfc() %>%
-    st_sf()
+  area.polygon <- sf::st_polygon(list(rbind(c(0, 0), c(poly.size, 0), c(poly.size, poly.size), c(0, poly.size), c(0, 0)))) %>%
+    sf::st_sfc() %>%
+    sf::st_sf()
 
   # create bounding box area and corresponding raster object
-  area.bbox <- st_bbox(area.polygon)
-  area.raster <- create_raster(area.bbox, tile.size = base.tile.size)
+  area.bbox <- sf::st_bbox(area.polygon)
+  area.raster <- mobloc::create_raster(area.bbox, tile.size = base.tile.size)
 
   # specify elevation raster --> default at 0 currently
   area.elevation <- area.raster
-  values(area.elevation) <- 0
+  raster::values(area.elevation) <- 0
 
   # retransform raster to sf for cell creation
-  base.tiles <- st_as_sf(st_as_stars(area.raster))
+  base.tiles <- sf::st_as_sf(stars::st_as_stars(area.raster))
 
   # build city geometry (1 obs.)
-  cities <- st_sample(area.polygon, city.num) %>%
-    st_buffer(dist = city.size, endCapStyle = city.shape) %>% # parameter if square or circle
-    st_geometry() %>%
-    st_union() %>%
-    st_sf() %>%
-    mutate(city = 1)
+  cities <- sf::st_sample(area.polygon, city.num) %>%
+    sf::st_buffer(dist = city.size, endCapStyle = city.shape) %>% # parameter if square or circle
+    sf::st_geometry() %>%
+    sf::st_union() %>%
+    sf::st_sf() %>%
+    dplyr::mutate(city = 1)
 
   # build hole geometry (1 obs.)
-  holes <- st_sample(cities, hole.num) %>%
-    st_buffer(dist = hole.size, endCapStyle = as.character(hole.shape)) %>% # parameter if square or circle
-    st_geometry() %>%
-    st_union() %>%
-    st_sf() %>%
-    mutate(hole = 1)
+  holes <- sf::st_sample(cities, hole.num) %>%
+    sf::st_buffer(dist = hole.size, endCapStyle = as.character(hole.shape)) %>% # parameter if square or circle
+    sf::st_geometry() %>%
+    sf::st_union() %>%
+    sf::st_sf() %>%
+    dplyr::mutate(hole = 1)
 
   # join tiles with cities and holes
   area.sf.helper <- base.tiles %>%
-    st_join(cities) %>%
-    st_join(holes) %>%
-    mutate(type = case_when(city == 1 & is.na(hole) ~ "Urban",
-                            city == 1 & hole == 1 ~ "Hole",
-                            TRUE ~ "Rural")) %>%
-    mutate(category = case_when(type %in% c("Urban", "Hole") ~ "Urban",
+    sf::st_join(cities) %>%
+    sf::st_join(holes) %>%
+    dplyr::mutate(type = dplyr::case_when(city == 1 & is.na(hole) ~ "Urban",
+                                          city == 1 & hole == 1 ~ "Hole",
+                                          TRUE ~ "Rural")) %>%
+    dplyr::mutate(category = dplyr::case_when(type %in% c("Urban", "Hole") ~ "Urban",
                                 type == "Rural" ~ "Rural")) %>%
-    rownames_to_column("tile.id") %>%
-    mutate(tile.id = as.integer(tile.id)) %>%
-    arrange(type)
+    tibble::rownames_to_column("tile.id") %>%
+    dplyr::mutate(tile.id = as.integer(tile.id)) %>%
+    dplyr::arrange(type)
 
   # find out how many tiles are of a certain type to match with respective pop-distribution function
   summary.area <- area.sf.helper %>%
-    st_drop_geometry() %>%
+    sf::st_drop_geometry() %>%
     dplyr::select(tile.id, type) %>%
-    group_by(type) %>%
-    summarise(n = n()) %>%
-    left_join(pop.dist.df, by = "type") %>%
-    mutate(final = paste0(expression, ", n = ", n, ")")) %>%
-    arrange(type)
+    dplyr::group_by(type) %>%
+    dplyr::summarise(n = dplyr::n()) %>%
+    dplyr::left_join(pop.dist.df, by = "type") %>%
+    dplyr::mutate(final = paste0(expression, ", n = ", n, ")")) %>%
+    dplyr::arrange(type)
 
   # sample pop vector from input distribution function
-  pop.helper <- unlist(map(summary.area$final, ~eval(parse(text = .x))))
+  pop.helper <- unlist(purrr::map(summary.area$final, ~eval(parse(text = .x))))
 
   # append pop to the sf data frame, apply necessary rounding and create final sf dataframe
   area.sf <- area.sf.helper %>%
-    mutate(pop = pop.helper) %>%
-    mutate(pop = round(pop, 0)) %>%
-    mutate(pop = if_else(pop < 0, 0, pop)) %>%
-    mutate(centroid.geometry = st_centroid(.$geometry)) %>%
-    mutate(X.centroid = unlist(map(.$centroid.geometry, 1)),
-           Y.centroid = unlist(map(.$centroid.geometry, 2))) %>%
+    dplyr::mutate(pop = pop.helper) %>%
+    dplyr::mutate(pop = round(pop, 0)) %>%
+    dplyr::mutate(pop = dplyr::if_else(pop < 0, 0, pop)) %>%
+    dplyr::mutate(centroid.geometry = sf::st_centroid(.$geometry)) %>%
+    dplyr::mutate(X.centroid = unlist(purrr::map(.$centroid.geometry, 1)),
+                  Y.centroid = unlist(purrr::map(.$centroid.geometry, 2))) %>%
     dplyr::select(tile.id, type, category, pop, X.centroid, Y.centroid) %>%
-    arrange(tile.id)
+    dplyr::arrange(tile.id)
 
   # create non-sf data frame version
   area.df <- area.sf %>%
-    st_drop_geometry() %>%
-    arrange(tile.id)
+    sf::st_drop_geometry() %>%
+    dplyr::arrange(tile.id)
 
   # put everything into a output list
   final <- list(area.params = area.params,
